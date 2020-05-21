5c9a193e5ba42e62d041ca82cef70aef077a146c
crimsonpenguin <49640122+crimsonpenguin@users.noreply.github.com>
2020-05-21 11:13:36 +0100

    Update post.py
    Updated script for Python 3

--- post.py
+++ post.py
@@ -1,16 +1,10 @@
-#pyhton 2.7 no addons or addtional modules installed
+#!/bin/python3
+import requests
+import json
 
-#!/usr/bin/python
-import urllib
-import httplib
+msg = 'test message'
 
-msg = 'test'
-
-slack_msg = json.dumps({'text':msg, 'icon_emoji':':smile:','username':'Test','channel':'#test'})
-link = httplib.HTTPSConnection('hooks.slack.com')
-#add full link for slack web hook
-web_hook_url = '/services/'
-headers={'Content-Type': 'application/json'}
-link.request("POST", web_hook_url, slack_msg, headers)
-resp = link.getresponse()
-ack = resp.read()
+slack_msg = json.dumps({'text':msg, 'icon_emoji':":smile:",'channel':'#general'})
+web_hook_url = 'https://hooks.slack.com/services/WEBHOOK'
+headers={'Content-Type': 'application/json; charset=UTF-8'}
+response = requests.post(web_hook_url, slack_msg, headers)
